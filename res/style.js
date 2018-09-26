0f5ef502ea4ec0fbdda069fc836414f97df58753
Logan Murphy <nagolyhprum@hotmail.com>
2018-09-26 19:16:55 -0400

    initial commit

---
+++ res/style.js
@@ -0,0 +1,38 @@
+export default {
+  bold_34: {
+    fontWeight: 'bold',
+    fontSize: 34
+  },
+  bold_20: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  medium_20: {
+    fontWeight: 'medium',
+    fontSize: 20
+  },
+  medium_16: {
+    fontWeight: 'medium',
+    fontSize: 16
+  },
+  medium_12: {
+    fontWeight: 'medium',
+    fontSize: 12
+  },
+  regular_20: {
+    fontSize: 20
+  },
+  regular_16: {
+    fontSize: 16
+  },
+  regular_14: {
+    fontSize: 14
+  },
+  regular_12: {
+    fontSize: 12
+  },
+  medium_40: {
+    fontWeight: 'medium',
+    fontSize: 40
+  }
+}
